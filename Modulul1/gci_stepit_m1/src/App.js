7b2e1338207da443beeb256daa45dc0cd7394096
Student <student@stepit.loc>
2019-02-02 12:44:18 +0200

    Updated

--- Modulul1/gci_stepit_m1/src/App.js
+++ Modulul1/gci_stepit_m1/src/App.js
@@ -4,26 +4,52 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            My first React code :)
-          </p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return (<div className="scoreboard">
+    <header>
+      <h1>Scoreboard</h1>
+      <span className ="stats">Players: 4</span>
+    </header>
+    <div className="player">
+      <span className="player-name"
+        ><button className="remove-player">✖</button>Guil</span
+      >
+      <div className="counter">
+        <button className="counter-action decrement">-</button
+        ><span className="counter-score">0</span>
+        <button className="counter-action increment">+</button>
       </div>
+    </div>
+    <div className="player">
+      <span className="player-name"
+        ><button className="remove-player">✖</button>Treasure</span
+      >
+      <div className ="counter">
+        <button className="counter-action decrement">-</button
+        ><span className="counter-score">0</span
+        ><button className="counter-action increment">+</button>
+      </div>
+    </div>
+    <div className="player">
+      <span className="player-name">
+        <button className="remove-player">✖</button>Ashley
+      </span>
+      <div className="counter">
+        <button className="counter-action decrement">-</button
+        ><span className="counter-score">0</span
+        ><button className="counter-action increment">+</button>
+      </div>
+    </div>
+    <div className="player">
+      <span className="player-name"
+        ><button className="remove-player">✖</button>James</span
+      >
+      <div className="counter">
+        <button className="counter-action decrement">-</button
+        ><span className="counter-score">0</span
+        ><button className="counter-action increment">+</button>
+      </div>
+    </div>
+  </div>
     );
   }
 }
